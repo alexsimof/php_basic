020b49c9d43981d14e6d4c04eb266e4f4770cec5
Alex Simonov <alex.simonof@gmail.com>
2024-05-20 15:02:46 +0300

    mysql

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
 <?php
+
+$dsn = 'mysql:host=mysql;port=3306;dbname=hillel';
+$database = new PDO($dsn, 'root', 'root');
+
+$stmt = $database->query("SELECT * FROM `teachers`");
+
+// Вопрос:
+//получается, когда мы делаем запрос через query то подготавливать его не надо?
+// он же подготавливает и выполняет ..
+
+function getName($stmt): Generator
+{
+    while ($row = $stmt->fetch()) {
+       yield $row['name'];
+    }
+}
+foreach (getName($stmt) as $row) {
+    echo '<br>';
+    print_r($row);
+    echo '<br>';
+}
+
+// вроде правильно все..)) с генератором
+
+$subject = 'biology';
+
+$sql1 = "SELECT * FROM `teachers` WHERE subject = ?";
+$stmt1 = $database->prepare($sql1);
+$stmt1->execute([$subject]);
+
+$res = $stmt1->fetch();
+
+echo "<pre>";
+print_r($res);
+echo "<pre>";
