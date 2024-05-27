adff8a5c7e953f0d271e1309a219175449e20e95
Alex Simonov <alex.simonof@gmail.com>
2024-05-27 16:53:48 +0300

    add script

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="server.php" method="get">
+        <input id="name" type="text" name="name" placeholder="Name">
+        <input id="email" type="email" name="email" placeholder="Email">
+        <input type="submit" value="Submit">
+    </form>
+
+    <form style="margin-top: 20px;" action="server.php" method="post">
+        <input id="name" type="number" name="number-one" placeholder="Enter number">
+        <input id="email" type="number" name="number-two" placeholder="Enter number">
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
---
+++ server.php
@@ -0,0 +1,27 @@
+<?php
+
+$methods = ['POST', 'GET'];
+
+$currentMethod = strtoupper($_SERVER['REQUEST_METHOD']);
+
+if (!in_array($currentMethod, $methods)) {
+    header('Location:http://localhost:8080/index.php', true, 302);
+    exit;
+}
+
+if ($currentMethod === 'GET' || !empty($name) || !empty($email)) {
+    $name = $_GET['name'];
+    $email = $_GET['email'];
+    echo 'Hello! This is are ' . $currentMethod . ' request method';
+    echo '<br>';
+    echo 'My name is ' . $name;
+    echo '<br>';
+    echo 'My email is ' . $email;
+
+} elseif ($currentMethod === 'POST' && isset($numberOne) && isset($numberTwo)) {
+    $numberOne = $_POST['number-one'];
+    $numberTwo = $_POST['number-two'];
+    $summa = $numberOne + $numberTwo;
+    echo $numberOne . '+' . $numberTwo . ' = ' . $summa;
+}
+echo 'Enter are number';
